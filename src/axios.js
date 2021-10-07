180613f144f2103b71c1f4a23e53bef144fdc802
Yash Patel <56584149+y4sh-patel@users.noreply.github.com>
2021-10-07 16:44:08 +0530

    Initial Commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+let url = "http://localhost:9000/api";
+if (process.env.NODE_ENV === "production") {
+    url = "https://vcpl.herokuapp.com/api";
+}
+
 const client = axios.create({
-    baseURL: "https://vcpl.herokuapp.com/api",
+    baseURL: url,
 });
 
 export default client;
